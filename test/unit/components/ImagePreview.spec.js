3b8bda90943ec6ab8a85d68917c25488a7b2e763
Henrik Fricke <henrik.fricke@jimdo.com>
2017-09-22 22:43:50 +0200

    Extend ImagePreview to handle upload

--- test/unit/components/ImagePreview.spec.js
+++ test/unit/components/ImagePreview.spec.js
@@ -9,23 +9,23 @@ Vue.use(Vuex);
 
 describe('ImagePreview', () => {
   let canvas;
+  let dispatch;
   let component;
-  let grayscale;
-  let sepia;
   let image;
+  let store;
   let saveButton;
 
   beforeEach(() => {
-    grayscale = 10;
-    sepia = 20;
-
-    const store = new Vuex.Store({
+    store = new Vuex.Store({
       getters: {
-        sepia: () => sepia,
-        grayscale: () => grayscale,
+        imageTools: () => [],
         imageSource: () => 'image/source.png',
       },
     });
+
+    dispatch = jasmine.createSpy('dispatch');
+    store.dispatch = dispatch;
+
     component = mount(ImagePreview, { globals: { $store: store } });
     image = component.find('img')[0];
     canvas = component.find('canvas')[0];
@@ -66,6 +66,30 @@ describe('ImagePreview', () => {
         expect(saveButton.getAttribute('download')).toEqual('image.jpg');
       });
     });
+
+    describe('upload input', () => {
+      let input;
+
+      beforeEach(() => {
+        input = component.find('input')[0];
+      });
+
+      it('should be present', () => {
+        expect(input).not.toBeUndefined();
+      });
+
+      it('should have `accept` attribute', () => {
+        expect(input.element.getAttribute('accept')).toEqual('.png, .jpg, .jpeg');
+      });
+
+      it('should handle onChange event', () => {
+        const uploadImage = jasmine.createSpy('uploadImage');
+        component.setMethods({ uploadImage });
+        input.trigger('change');
+
+        expect(uploadImage).toHaveBeenCalled();
+      });
+    });
   });
 
   describe('methods', () => {
@@ -82,6 +106,55 @@ describe('ImagePreview', () => {
         component.vm.saveImage();
 
         expect(saveButton.getAttribute('href')).toEqual(dataURL);
+      });
+    });
+
+    describe('uploadImage', () => {
+      it('should decline invalid files', () => {
+        const event = {
+          target: {
+            files: [
+              {
+                type: 'image/gif',
+              },
+            ],
+          },
+        };
+
+        component.vm.uploadImage(event);
+
+        expect(dispatch).not.toHaveBeenCalled();
+      });
+
+      it('should dispatch base 64 encoded image', () => {
+        const event = {
+          target: {
+            files: [
+              {
+                type: 'image/jpeg',
+              },
+            ],
+          },
+        };
+
+        const fileReaderMock = {
+          readAsDataURL: jasmine.createSpy('readAsDataURL'),
+          addEventListener: jasmine.createSpy('addEventListener'),
+          result: '<-- a base 64 encoded image -->',
+        };
+
+        spyOn(window, 'FileReader').and.returnValue(fileReaderMock);
+
+        component.vm.uploadImage(event);
+        expect(fileReaderMock.readAsDataURL).toHaveBeenCalledWith(event.target.files[0]);
+        expect(fileReaderMock.addEventListener).toHaveBeenCalledWith('load', jasmine.any(Function));
+
+        const listener = fileReaderMock.addEventListener.calls.mostRecent().args[1];
+        listener();
+
+        expect(store.dispatch).toHaveBeenCalledWith('updateImageSource', {
+          imageSource: fileReaderMock.result,
+        });
       });
     });
   });
